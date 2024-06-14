d4986cfba58692ef0fd0dac7972238bd99e35c9b
Pkaur427 <pk6345422@gmail.com>
2024-06-14 00:55:46 -0400

    lab2 Completed

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Author: Parminder kaur
+# Author ID: 142566215
+# Date Created: 2024/06/1
+
+timer = 10
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
+
---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Parminder Kaur
+# Author ID: 142566215
+# Date Created: 2024/06/1
+
+import sys
+
+timer = int(sys.argv[1])
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
+
